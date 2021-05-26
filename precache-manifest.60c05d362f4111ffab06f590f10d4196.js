663a134288708afd908b863e442ed24840f4bd69
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-26 19:24:52 +0000

    Deploy hemanditwiz/minimal-sudoku to github.com/hemanditwiz/minimal-sudoku.git:gh-pages

---
+++ precache-manifest.60c05d362f4111ffab06f590f10d4196.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd65bedf38fc8e4ee9b0a0a1f3670a5",
+    "url": "/minimal-sudoku/5780aa5e422dddc342c4.module.wasm"
+  },
+  {
+    "revision": "2c6add7bdabe59b4aa3f",
+    "url": "/minimal-sudoku/95eaf149d868a472ba31.module.wasm"
+  },
+  {
+    "revision": "68965119377a0ed05e426a2880bd1a52",
+    "url": "/minimal-sudoku/index.html"
+  },
+  {
+    "revision": "ef47bc2d362cbeff04f80d1c77054769",
+    "url": "/minimal-sudoku/static/js/1.680fbd29.chunk.worker.js"
+  },
+  {
+    "revision": "6701bea106e00f808e24",
+    "url": "/minimal-sudoku/static/js/2.5ab41f44.chunk.js"
+  },
+  {
+    "revision": "64faa410d4e8fd5e1cc94b7c8f4eec99",
+    "url": "/minimal-sudoku/static/js/2.5ab41f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6add7bdabe59b4aa3f",
+    "url": "/minimal-sudoku/static/js/3.3cbb3afb.chunk.js"
+  },
+  {
+    "revision": "ddc185c536f77df6302b",
+    "url": "/minimal-sudoku/static/js/main.637301d5.chunk.js"
+  },
+  {
+    "revision": "15f20c6d357254b19d43",
+    "url": "/minimal-sudoku/static/js/runtime-main.794f06d6.js"
+  },
+  {
+    "revision": "395df21c5762a017ea65f2f630fb32cd",
+    "url": "/minimal-sudoku/static/js/sudoku.worker.8a5d7f82.worker.js"
+  },
+  {
+    "revision": "3afcf0e0ae130b6f03c8150ed133ca33",
+    "url": "/minimal-sudoku/static/media/moon.3afcf0e0.svg"
+  },
+  {
+    "revision": "fe4053b9ad1356c378185ae6c36b2845",
+    "url": "/minimal-sudoku/static/media/sun.fe4053b9.svg"
+  }
+]);
